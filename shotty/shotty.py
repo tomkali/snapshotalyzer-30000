4280cb0508cc3334fed5962d076f9b51c3c06d0b
Tom Kali <thomas.kali@blackstone.com>
2020-02-10 17:16:36 -0500

    Filter instances by project.  Also, stop and start instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,18 +4,62 @@ import click
 session = boto3.Session(profile_name='shotty')
 ec2 = session.resource('ec2')
 
-@click.command()
-def list_instances():
+@click.group()
+def instances():
+	"""Command for instances"""
+
+@instances.command ('list')
+@click.option('--project', default=None, help="Only instances for this project (tag Project:<name>)")
+
+def list_instances(project):
 	"List EC2 instances"
-	for i in ec2.instances.all():
-	    print (', '.join ((
-	    	i.id,
-	    	i.instance_type,
-	    	i.placement['AvailabilityZone'],
-	    	i.state['Name'],
-	    	i.public_dns_name)))
+	instances = get_instances(project)
+
+
+#	if project:
+#		filters = [{'Name':'tag:Project', 'Values':["Valkyrie"]}]
+#		instances = ec2.instances.filter(Filters=filters)
+#	else:
+#		instances = ec2.instances.all()
+
+
+	for i in instances:
+		tags = { t['Key']: t['Value'] for t in i.tags or [] }
+
+		print (', '.join ((
+			i.id,
+			i.instance_type,
+			i.placement['AvailabilityZone'],
+			i.state['Name'],
+			i.public_dns_name,
+			tags.get('Project', '<no project>'))))
 
 	return
 
+@instances.command('start')
+@click.option('--project', default=None, help='Only instances for project')
+
+def start_instances(project):
+	"start EC2 instances"
+
+	instances = get_instances(project)
+
+	for i in instances:
+		print ("starting {0}...".format(i.id))
+		i.start()
+
+	return
+
+def get_instances (project):
+	instances = []
+
+	if project:
+		filters = [{'Name':'tag:Project', 'Values':["Valkyrie"]}]
+		instances = ec2.instances.filter(Filters=filters)
+	else:
+		instances = ec2.instances.all()
+
+	return instances
+
 if __name__ == '__main__':
-	list_instances()
+	instances()
